9642579d19bf84d41ca9f2c62b1dec503a8bfa8f
Erick-pais <erick.pais12@gmail.com>
2023-10-23 21:57:07 -0300

    botao excluir

--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,73 @@
 import React from "react";
-import { StyleSheet, 
-        Text, 
-        TextInput, 
-        View,
-        TouchableOpacity,
-        ScrollView,
-    FlatList } from "react-native";
-interface Tarefa{
+import {
+    StyleSheet, 
+    Text, 
+    TextInput, 
+    View, 
+    TouchableOpacity, 
+    ScrollView,
+    FlatList
+} from "react-native";
+
+interface Tarefa {
     id: string;
-    title:string;
+    title: string;
 }
 
 export const Home = () => {
-    {/*Colchete para usa array*/}
+
+    {/*[] - array*/}
     const [newTask, setNewTask] = React.useState('');
-    const [tarefas, setTarefas] = React.useState<Tarefa[]>([]);
+    const [tarefas, setTarefas] = React.useState<Tarefa[]>([]);      
 
-    const adcTarefa = () => {
-        const dados = {
-            id: String(new Date().getTime()),
-            title: newTask ? newTask : 'Nova tarefa',
-        };
-        setTarefas([... tarefas,dados])
-    }
-
+        const adcTarefa = () => {
+            const dados = {
+                    id: String(new Date().getTime()),
+                    title: newTask ? newTask : 'Registro vazio',
+                };
+                setTarefas([...tarefas, dados]);
+                setNewTask("");
+            };
+        const removerTarefa = (id: string) => {
+            const tarefasAtualizadas = tarefas.filter((tarefa) =>
+            tarefa.id != id );
+            setTarefas(tarefasAtualizadas);
+        }
+        
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Tarefas do dia</Text>
-            <TextInput onChangeText={setNewTask} placeholderTextColor='#555' placeholder="Tarefas" style={styles.fields} />
-            
+            <TextInput onChangeText={setNewTask} value={newTask} placeholderTextColor='#555' placeholder="Tarefas" style={styles.fields} />
             <TouchableOpacity onPress={adcTarefa} activeOpacity={0.7} style={styles.button}>
-                <Text style={styles.buttonText}>enviar</Text>
-                
-        
-        </TouchableOpacity>
-
-        {/*Scroolbar*/}
-        {/*<ScrollView>
-        {tarefas.map((tarefa : Tarefa) => (
-            <TouchableOpacity key={tarefa.id}>
-                <Text style={styles.buttonText}>{tarefa.title}</Text>
-            </TouchableOpacity>))}         
-                <Text style={styles.title}>Lista de tarefa</Text>
-        </ScrollView>*/}
-        
-        <FlatList
-            data={tarefas}
-            keyExtractor={(item: Tarefa) => item.id}
-            renderItem={({item} : {item: Tarefa}) => (
-            <TouchableOpacity>
-                <Text style={styles.buttonText} > {item.title}</Text>
+                <Text style={styles.buttonText}>Entrar</Text>
             </TouchableOpacity>
-            )}
-        />
+            <Text style={styles.title}>Lista de tarefas</Text>
+
+            {/*Scrollbar*/}
+            {/*<ScrollView>
+                {tarefas.map((tarefa : Tarefa) => (
+                    <TouchableOpacity key={tarefa.id}>
+                        <Text style={styles.buttonText}>{tarefa.title}</Text>
+                    </TouchableOpacity>
+                ))}
+                </ScrollView>*/}
+
+            <FlatList
+                data={tarefas}
+                keyExtractor={(item: Tarefa) => item.id}
+                renderItem={({item} : {item: Tarefa}) => (
+                    <TouchableOpacity 
+                    onPress={() => removerTarefa(item.id)}
+                    style={styles.renderList}>
+                        <Text style={styles.buttonText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
+
         </View>
     );
-};
-    
-
-    
+}; 
 
 const styles = StyleSheet.create({
     container: {
@@ -67,6 +75,13 @@ const styles = StyleSheet.create({
       backgroundColor: '#121214',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    renderList:{
+        backgroundColor: 'green',
+        padding: 10,
+        marginTop: 10 , 
+        borderRadius: 50,
+        alignItems: 'center',
     },
     title: {
         color: '#f1f1f1',
